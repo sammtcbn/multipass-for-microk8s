0f02710ac723899b3ac6b1492f97361f6105d858
sammtcbn <sammtcbn@gmail.com>
2022-07-05 16:01:22 +0800

    prompt command

--- create_3_node_cluster.bash
+++ create_3_node_cluster.bash
@@ -1,6 +1,15 @@
 #!/bin/bash
+echo sudo snap install multipass --classic
 sudo snap install multipass --classic
+
+echo multipass launch -c 2 -m 2G -d 8G -n node1
 multipass launch -c 2 -m 2G -d 8G -n node1
+
+echo multipass launch -c 2 -m 2G -d 8G -n node2
 multipass launch -c 2 -m 2G -d 8G -n node2
+
+echo multipass launch -c 2 -m 2G -d 8G -n node3
 multipass launch -c 2 -m 2G -d 8G -n node3
+
+echo multipass list
 multipass list
--- delete_3_node_cluster.bash
+++ delete_3_node_cluster.bash
@@ -1,6 +1,15 @@
 #!/bin/bash
+echo multipass delete node1
 multipass delete node1
+
+echo multipass delete node2
 multipass delete node2
+
+echo multipass delete node3
 multipass delete node3
+
+echo multipass purge
 multipass purge
+
+echo multipass list
 multipass list
